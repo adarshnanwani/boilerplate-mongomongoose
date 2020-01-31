3d0665c373798bd7e9feacbd5f82f0117e191708
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-01-31 19:45:57 +0530

    Add person model

--- myApp.js
+++ myApp.js
@@ -15,10 +15,8 @@
 //
 // mongoose.connect(<Your URI>, { useNewUrlParser: true, useUnifiedTopology: true });
 var mongoose = require("mongoose");
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+console.log(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI);
 
 var Schema = mongoose.Schema;
 
